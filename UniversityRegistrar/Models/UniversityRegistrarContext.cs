d9c92dcf6fc8275ee961bc7f0f7da0dd6d99966f
“Jamie <knutsenjamie@yahoo.com>
2021-05-25 11:21:11 -0700

    add initial migration Co-authored-by: Jesse White <jesse.white6@gmail.com> Co-Authored-By: Michael Burton <micbur1@gmail.com> Co-authored-by: Brent Hubbard <hubbardbrent@hotmail.com>

--- UniversityRegistrar/Models/UniversityRegistrarContext.cs
+++ UniversityRegistrar/Models/UniversityRegistrarContext.cs
@@ -1,14 +1,14 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace ToDoList.Models
+namespace UniversityRegistrar.Models
 {
-  public class ToDoListContext : DbContext
+  public class UniversityRegistrarContext : DbContext
   {
-    public virtual DbSet<Category> Categories { get; set; }
-    public DbSet<Item> Items { get; set; }
-    public DbSet<CategoryItem> CategoryItem { get; set; }
+    public virtual DbSet<Course> Courses { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<CourseStudent> CourseStudent { get; set; }
 
-    public ToDoListContext(DbContextOptions options) : base(options) { }
+    public UniversityRegistrarContext(DbContextOptions options) : base(options) { }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
@@ -17,6 +17,4 @@ namespace ToDoList.Models
   }
 }
 
-//Obviously change out namespace, class, and Context file name to whatever is used as the project name instead of ToDoList.
-
-// Change out Item/Items and Category/Categories to match what is used in the project. REMEMBER TO MAKE NOTE OF SINGULAR VS PLURAL USE. 
+ 
